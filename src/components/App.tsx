c52bd13153e00b2d4a04195250b8f9fd2042a53e
voldemorten <mortendlaursen@gmail.com>
2019-05-28 23:04:39 +0900

    demo is done

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,10 +1,12 @@
 import * as React from "react";
 
-import demo from "../logic/logic";
+import * as Logic from "../logic/logic";
 
 export class App extends React.Component {
     render() {
-        demo();
+        let g = Logic.construct_demo_graph();
+        let ff = Logic.compute_max_flow(g, 0, 9);
+        console.log(ff);
         return <h1>Hello</h1>;
     }
 }
--- src/logic/logic.ts
+++ src/logic/logic.ts
@@ -1,3 +1,39 @@
-export default function demo() {
-    console.log("Still runs!");
+var jsgraphs = require('js-graph-algorithms');
+
+export function construct_demo_graph() {
+    let students = require('../data/students.json').students;
+    let timeslots = require('../data/timeslots.json').timeslots;
+        //node 0 should be source and node [student.length + timeslots.length + 2] should be sink
+    let size = students.length + timeslots.length + 2
+    let g = new jsgraphs.FlowNetwork(size);
+    // console.log(g);
+    //create edges from source to every student
+    for(let i = 0; i<students.length; i++) {
+        g.addEdge(new jsgraphs.FlowEdge(0, i+1, 100));
+        // console.log("student: ", students[i]);
+        for(let j = 0; j<students[i].preferences.length; j++) {
+            //add edges from each student to every prefered timeslot
+            // console.log("i: ",i," i+1: ", i+1);
+            // console.log("j: ", j);
+            // console.log("preference: ", students[i].preferences[j]);
+            // console.log("students.length+students[i].preferences[j]: ", students.length+students[i].preferences[j]);
+            g.addEdge(new jsgraphs.FlowEdge(i+1, students.length+students[i].preferences[j], 1));
+        }
+    }
+    // add edges from timeslots to sink
+    for(let k = 0; k<timeslots.length; k++) {
+        // console.log(students.length+k+1," ", size-1, " ", timeslots[k].capacity);
+        g.addEdge(new jsgraphs.FlowEdge(students.length+k+1, size-1, timeslots[k].capacity));
+    }
+    return g;
 }
+
+export function compute_max_flow(g: Object, source: number, sink: number) {
+    return new jsgraphs.FordFulkerson(g, source, sink);
+}
+
+export function export_demo() {
+    console.log("demo");
+}
+
+export default construct_demo_graph
