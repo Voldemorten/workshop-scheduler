69203166e0b7f603f691e92509c8fcc08f216789
voldemorten <mortendlaursen@gmail.com>
2019-06-03 17:35:47 +0900

    almost done

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -4,9 +4,31 @@ import * as Logic from "../logic/logic";
 
 export class App extends React.Component {
     render() {
-        let g = Logic.construct_demo_graph();
-        let ff = Logic.compute_max_flow(g, 0, 9);
-        console.log(ff);
+        let penalty = Infinity;
+        let opt_solution;
+        let org_graph = Logic.construct_demo_graph();
+        for(let i = 0; i<10; i++) {
+            let g = org_graph;
+            let ff = Logic.compute_max_flow(g, 0, 7 );
+            console.log(ff);
+            if(ff.value == g['max_capacity']) {
+                //output each solution
+                let solution = Logic.convert_to_solution(g);
+                console.log("Solution " + i + "\n------------------------");
+                Logic.print_solution(solution);
+
+                //used to find the best solution
+                if(solution['penalty'] < penalty) {
+                    opt_solution = solution;
+                }
+            } else {
+                console.log("No feasible solution");
+                break;
+            }
+        }
+        console.log("\nOptimal solution:\n------------------------");
+        Logic.print_solution(opt_solution);
+
         return <h1>Hello</h1>;
     }
 }
--- src/logic/logic.ts
+++ src/logic/logic.ts
@@ -1,39 +1,97 @@
-var jsgraphs = require('js-graph-algorithms');
+import { node } from "prop-types";
+import { objectTypeAnnotation } from "@babel/types";
+
+var jsgraphs = require('./jsgraphs');
 
 export function construct_demo_graph() {
     let students = require('../data/students.json').students;
     let timeslots = require('../data/timeslots.json').timeslots;
-        //node 0 should be source and node [student.length + timeslots.length + 2] should be sink
+    //node 0 should be source and node [student.length + timeslots.length + 2] should be sink
     let size = students.length + timeslots.length + 2
     let g = new jsgraphs.FlowNetwork(size);
-    // console.log(g);
+    
+    //to check if solution is feasible
+    g.max_capacity = 0;
+
+    //labeling
+    g.node(0).label = "source";
+    g.node(0).type = "source"
+    g.node(size-1).label = "sink";
+    g.node(size-1).type = "sink";
+    
     //create edges from source to every student
     for(let i = 0; i<students.length; i++) {
         g.addEdge(new jsgraphs.FlowEdge(0, i+1, 100));
-        // console.log("student: ", students[i]);
+        
+        //labeling 
+        g.node(i+1).label = students[i].name;
+        g.node(i+1).type = "student"
+        g.edge(0, i+1).label = "From source to " + students[i].name;
+        
         for(let j = 0; j<students[i].preferences.length; j++) {
             //add edges from each student to every prefered timeslot
-            // console.log("i: ",i," i+1: ", i+1);
-            // console.log("j: ", j);
-            // console.log("preference: ", students[i].preferences[j]);
-            // console.log("students.length+students[i].preferences[j]: ", students.length+students[i].preferences[j]);
             g.addEdge(new jsgraphs.FlowEdge(i+1, students.length+students[i].preferences[j], 1));
+            // labeling
+            g.edge(i+1, students.length+students[i].preferences[j]).label = "From " + students[i].name + " to timeslot #"+students[i].preferences[j];
         }
     }
     // add edges from timeslots to sink
     for(let k = 0; k<timeslots.length; k++) {
-        // console.log(students.length+k+1," ", size-1, " ", timeslots[k].capacity);
+        g.max_capacity += timeslots[k].capacity; 
+
         g.addEdge(new jsgraphs.FlowEdge(students.length+k+1, size-1, timeslots[k].capacity));
+
+        //labeling
+        g.edge(students.length+k+1, size-1).label = "From timeslot#" + (k+1) + " to sink";
+        g.node(students.length+k+1).label = "timeslot #" + (k+1);
+        g.node(students.length+k+1).type = "timeslot";
     }
     return g;
 }
 
-export function compute_max_flow(g: Object, source: number, sink: number) {
+export function compute_max_flow(g, source: number, sink: number) {
     return new jsgraphs.FordFulkerson(g, source, sink);
 }
 
+export function convert_to_solution(g) {
+    let out = {}
+    out["solution"] = {}
+    out["penalty"] = 0;
+    for(let i = 0; i<g.adjList.length; i++) {
+        let adjlist = g.adjList[i];
+        // to compute penalty
+        let end_nodes = [];
+        for(let j = 0; j<adjlist.length; j++) {
+            if(adjlist[j].flow > 0 && adjlist[j].v == i && g.node(adjlist[j].v).type == "student") {
+                end_nodes.push(adjlist[j].w);
+                let from = g.node(adjlist[j].v).label;
+                let to = g.node(adjlist[j].w).label;
+                if(!out["solution"][from]) {
+                    out["solution"][from] = [to]
+                } else {
+                    out["solution"][from].push(to);
+                }
+            }
+        }
+
+        for(let k = end_nodes.length-1; k>0; k--) {
+            let diff = end_nodes[k] - end_nodes[k-1];
+            if (diff > 1) {
+                out['penalty'] += diff-1;
+            }
+        }
+    }
+    return out;
+};
+
+export function print_solution(solution) {
+    console.table(solution['solution']);
+    if(solution['penalty']>0) console.error("Penalty: " + solution['penalty'] + "\n\n");
+    else console.log("Penalty: " + solution['penalty'] + "\n\n");
+};
+
 export function export_demo() {
     console.log("demo");
-}
+};
 
 export default construct_demo_graph
